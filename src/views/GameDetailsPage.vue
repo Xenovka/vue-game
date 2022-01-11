c8f8918daa0a8bf8c9ec8bf3aabbb6974b46c01c
Xenovka <wendyanto18@gmail.com>
2022-01-11 18:30:00 +0700

    fix a single stupid bug and showing the game details

--- src/views/GameDetailsPage.vue
+++ src/views/GameDetailsPage.vue
@@ -1,6 +1,9 @@
 <template>
-  <div class="game-details__wrapper">
-    <h1 class="game-details__name">{{ gameDetails.name }}</h1>
+  <div class="game-details" v-if="gameDetails">
+    <img class="game-details__background-image" :src="gameDetails['background_image']" alt="game image" />
+    <div class="game-details__content-wrapper container py-5">
+      <h1 class="game-details__name">{{ gameDetails.name }}</h1>
+    </div>
   </div>
 </template>
 
@@ -19,6 +22,7 @@ export default {
 
     store.dispatch("getGameDetailsBySlug", gameSlug).then(() => {
       gameDetails.value = store.state.gameDetails[0];
+      console.log(gameDetails.value);
     });
 
     return {
@@ -27,3 +31,22 @@ export default {
   }
 };
 </script>
+
+<style lang="scss">
+.game-details {
+  &__background-image {
+    position: absolute;
+    width: 100vw;
+    height: 100vh;
+    filter: brightness(0.2);
+    z-index: -1;
+  }
+
+  &__name {
+    font-size: 4rem;
+    font-weight: 700;
+    text-align: center;
+    color: #fff;
+  }
+}
+</style>
