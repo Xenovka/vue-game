f81d984ef2458631a0f40125fa26f3b094aac6c4
Xenovka <wendyanto18@gmail.com>
2022-01-13 19:59:00 +0700

    change navbar file name

---
+++ src/components/HomeNavbar.vue
@@ -0,0 +1,42 @@
+<template>
+  <div class="default-navbar">
+    <router-link to="/" class="default-navbar__brand">D N E V</router-link>
+    <SearchInputForm />
+    <GenresListDropdown />
+  </div>
+</template>
+
+<script>
+import SearchInputForm from "./SearchInputForm.vue";
+import GenresListDropdown from "./GenresListDropdown.vue";
+
+export default {
+  components: {
+    SearchInputForm,
+    GenresListDropdown
+  }
+};
+</script>
+
+<style lang="scss">
+.default-navbar {
+  width: 100%;
+  display: flex;
+  align-items: center;
+  margin: 1rem 0 3rem;
+
+  &__brand {
+    text-decoration: none;
+    color: #fff;
+    font-weight: 900;
+    font-size: 1.8rem;
+    letter-spacing: 2px;
+    display: inline-block;
+    padding-right: 2rem;
+
+    &:hover {
+      color: #fff;
+    }
+  }
+}
+</style>
